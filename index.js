0797dd44758d58f5a3744793269b8c26445db77c
Eric Lau <yasuyo369@gmail.com>
2017-11-13 09:18:36 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector);
+}
+
+function nestedTarget()
+{}
+
+function increaseRankBy(n)
+{
+  
+}
+
+function deepestChild()
